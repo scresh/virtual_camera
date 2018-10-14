12f321a5fb5983015cc7d64cc9b00fd18eebf33a
scresh <screshowski@gmail.com>
2018-10-14 16:10:31 +0200

    Add rotation

--- geometry.py
+++ geometry.py
@@ -1,15 +1,59 @@
-from numpy import full, cos, sin, pi
+from numpy import full, cos, sin, pi, matmul, array
 import cv2
 
 
-def project(point, observer, x_center, y_center, focal_length):
+def rotate(p, a_x, a_y, a_z):
+    sin_x = sin(a_x * (pi / 180))
+    cos_x = cos(a_x * (pi / 180))
+
+    sin_y = sin(a_y * (pi / 180))
+    cos_y = cos(a_y * (pi / 180))
+
+    sin_z = sin(a_z * (pi / 180))
+    cos_z = cos(a_z * (pi / 180))
+
+    rx = array([
+        [1.0, 0.0, 0.0],
+        [0.0, cos_x, -sin_x],
+        [0.0, sin_x, cos_x],
+    ])
+
+    ry = array([
+        [cos_y, 0.0, sin_y],
+        [0.0, 1.0, 0.0],
+        [-sin_y, 0.0, cos_y],
+    ])
+
+    rz = array([
+        [cos_z, -sin_z, 0.0],
+        [sin_z, cos_z, 0.0],
+        [0.0, 0.0, 1.0],
+    ])
+
+    r = matmul(matmul(matmul(rx, ry), rz), p)
+
+    return r
+
+
+def project(point, observer, angles, focal_length, center):
     p_x, p_y, p_z = point
     o_x, o_y, o_z = observer
+    a_x, a_y, a_z = angles
+
+    x_center, y_center = center
 
     x = p_x - o_x
     y = p_y - o_y
     z = o_z - p_z
- 
+
+    p = array([x, y, z])
+    r = rotate(p, a_x, a_y, a_z)
+
+    x = r[0]
+    y = r[1]
+    z = r[2]
+
+    # Draw visible points only
     if z > 0:
         of = focal_length / z
     else:
@@ -20,7 +64,6 @@ def project(point, observer, x_center, y_center, focal_length):
 
     x = x_center + x
     y = y_center - y
-
     return x, y
 
 
@@ -48,14 +91,17 @@ class Canvas:
             b_point = figure.points[pair[1]]
             self.pairs.append((self.points.index(a_point), self.points.index(b_point)))
 
-    def get_image(self, cam_x, cam_y, cam_z, focal_length):
+    def get_image(self, cam_x, cam_y, cam_z, ang_x, ang_y, ang_z, f):
         self.image = full((self.width, self.height, 1), 255.0)
 
         x_center = int(self.width / 2)
         y_center = int(self.height / 2)
-        o = (cam_x, cam_y, cam_z)
 
-        points_2d = [*map(lambda p: project(p, o, x_center, y_center, focal_length), self.points)]
+        c = (x_center, y_center)
+        o = (cam_x, cam_y, cam_z)
+        a = (ang_x, ang_y, ang_z)
+
+        points_2d = [*map(lambda p: project(p, o, a, f, c), self.points)]
 
         for pair in self.pairs:
             a_index = pair[0]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-from geometry import Canvas, Figure
+from geometry import Canvas, Figure, rotate
 
 
 def main():
@@ -25,6 +25,10 @@ def main():
         )
     )
 
+    ang_x = 0
+    ang_y = 0
+    ang_z = 0
+
     cam_x = 256
     cam_y = 256
     cam_z = 256
@@ -32,30 +36,55 @@ def main():
     focal_length = 256
 
     canvas = Canvas()
-    # canvas.add_figure(axes)
+    canvas.add_figure(axes)
     canvas.add_figure(tetrahedron)
     canvas.add_figure(cube)
 
     while True:
-        cv2.imshow('image', canvas.get_image(cam_x, cam_y, cam_z, focal_length))
-
+        cv2.imshow('image', canvas.get_image(cam_x, cam_y, cam_z, ang_x, ang_y, ang_z, focal_length))
         k = cv2.waitKey(0)
-        if k == ord('6'):
-            cam_x += 2
-        elif k == ord('4'):
-            cam_x -= 2
-        elif k == ord('8'):
-            cam_y += 2
-        elif k == ord('2'):
-            cam_y -= 2
+
+        # Shift matrix
+        p = [0.0, 0.0, 0.0]
+
+        # Translation
+        if k == 83:
+            p = [2.0, 0.0, 0.0]
+        elif k == 81:
+            p = [-2.0, 0.0, 0.0]
+        elif k == 82:
+            p = [0.0, 2.0, 0.0]
+        elif k == 84:
+            p = [0.0, -2.0, 0.0]
         elif k == ord('-'):
-            cam_z += 2
+            p = [0.0, 0.0, 2.0]
         elif k == ord('+'):
-            cam_z -= 2
+            p = [0.0, 0.0, -2.0]
+
+        # Rotation
+        elif k == ord('8'):
+            ang_x += 2
+        elif k == ord('2'):
+            ang_x -= 2
+        elif k == ord('4'):
+            ang_y += 2
+        elif k == ord('6'):
+            ang_y -= 2
+        elif k == ord('7'):
+            ang_z += 2
+        elif k == ord('9'):
+            ang_z -= 2
+
+        # Focal length size
         elif k == ord('['):
             focal_length *= 1.1
         elif k == ord(']'):
             focal_length /= 1.1
+
+        r = rotate(p, ang_x, ang_y, ang_z)
+        cam_x = int(round(cam_x + r[0]))
+        cam_y = int(round(cam_y + r[1]))
+        cam_z = int(round(cam_z + r[2]))
 
 
 if __name__ == '__main__':
